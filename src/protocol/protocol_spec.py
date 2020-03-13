a3935b41bc86590c46b59c1dfed912f0d899cc46
nmramorov <nmramorov@protonmail.com>
2020-03-13 18:21:29 +0500

    refactored code of Serial_Protocol class

--- src/protocol/protocol_spec.py
+++ src/protocol/protocol_spec.py
@@ -18,47 +18,13 @@ class Serial_Protocol:
     the given serial port
     """
 
-    def __init__(self):   
-
-        self.Package_data = namedtuple("Package_data", "package_id\
-                                                    package_len\
-                                                    day\
-                                                    month\
-                                                    year\
-                                                    date_font_color\
-                                                    date_back_color\
-                                                    hour\
-                                                    minute\
-                                                    second\
-                                                    clock_font_color\
-                                                    clock_back_color\
-                                                    control_sum")
-
+    def __init__(self, **kwargs):
+        self.user_input = kwargs
     
     def init_package(self):
-
-
-    def create_package(self) -> namedtuple:
-        """Creates package for serial port, package contains data in bytes"""
-        
-        package = self.Package_data(
-            current_date=date.today(),
-            current_day=str(date.day),
-            current_time=datetime.now(),
-            font_color='Green',
-            background_color='Blue'
-        )
-
-        logger.info(f"The package data is:\n\
-                    Current date is {package.current_date}\n\
-                    Current day is {package.current_day}\n\
-                    Current time is {package.current_time}\n\
-                    Current font is {package.font_color}\n\
-                    Current background is {package.background_color}")
-        return package
+        p = Package(self.user_input)
+        return p.create_package()
 
 
 if __name__ == "__main__":
-    Test_Protocol = Serial_Protocol(10, ('test_font', 'test_background'))
-    test_package = Test_Protocol.create_package()
-    logger.info(test_package)
+    pass
