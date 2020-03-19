064b30df85e69a6f6d500e91420249c916a3ee28
nmramorov <nmramorov@protonmail.com>
2020-03-19 18:40:31 +0500

    added encoding tables, slightly refactored code

--- src/gui/interface.py
+++ src/gui/interface.py
@@ -6,13 +6,49 @@ from src.protocol.protocol_spec import Serial_Protocol
 
 
 PACKAGE_DATA_LENGTH = 88
+RGB_COLOR_DEFINITIONS = dict(
+    black   =    '#000000',
+    navy    =    '#000080',
+    dark_green = '#008000',
+    dark_cyan  = '#008080',
+    maroon   =   '#800000',
+    purple   =   '#800080',
+    olive    =   '#808000',
+    light_grey = '#c0c0c0',
+    dark_grey='#808080',
+    blue='#0000ff',
+    green='#00ff00',
+    cyan='#00ffff',
+    red='#ff0000',
+    magenta='#ff00ff',
+    yellow='#ffff00',
+    white='#ffffff',
+)
+RGB_ENCODED_COLORS = dict(
+    black=0,
+    navy=1,
+    dark_green=2,
+    dark_cyan=3,
+    maroon=4,
+    purple=5,
+    olive=6,
+    light_grey=7,
+    dark_grey=8,
+    blue=9,
+    green=10,
+    cyan=11,
+    red=12,
+    magenta=13,
+    yellow=14,
+    white=15,
+)
+
 
 class GUIException(Exception):
     pass
 
 
 class GUI(QWidget):
-    
     def __init__(self):
         super().__init__()
         self.user_data = dict(
@@ -23,13 +59,11 @@ class GUI(QWidget):
             clock_back_color=0
         )
         self.package = ''
-        
+        self.label = QLabel()
         self.initUI()
 
-        
-    def initUI(self):
-        self.label = QLabel()
 
+    def initUI(self):
         self.generateButtons()
         self.setButtonsActivities()
 
@@ -39,6 +73,7 @@ class GUI(QWidget):
         self.setGeometry(300, 300, 800, 500)
         self.setWindowTitle('Mramorov-Volkov-Martinova')    
         self.show()
+
 
     def generatePackage(self):
         p = Serial_Protocol(self.user_data['data_len'],
@@ -50,18 +85,25 @@ class GUI(QWidget):
         self.label.setText('Пакет создан')
         self.showPackageInfoButton.setDisabled(0)
 
+
     def showPackageInfo(self):
         self.label.setText(str(self.package))
+
 
     def showCOMDialog(self):
         text, ok = QInputDialog.getText(self, 'Настройка COM порта', 'Введите параметр:')
 
+
     def showColorDialog(self):
         self.openColorDialog()
 
+
     def openColorDialog(self):
         color = QColorDialog.getColor()
+        # self.encodeColor(color.name())
+        # print(self.user_data)
         if self.setDateFontColorButton.isActiveWindow():
+            print(1)
             self.user_data['date_font_color'] = color.name()
         elif self.setDateBackColorButton.isActiveWindow():
             self.user_data['date_back_color'] = color.name()
@@ -70,8 +112,26 @@ class GUI(QWidget):
         elif self.setClockBackButton.isActiveWindow():
             self.user_data['clock_back_color'] = color.name()
 
-    def encodeColors(self, color):
-        pass
+
+    # def encodeColor(self, color_name):
+    #     if color_name in RGB_COLOR_DEFINITIONS.values():
+    #         # the code below returns key from dict according to the given value of the dict
+    #         # this is used to map keys in two dicts with RGB colors: RGB_COLOR_DEFINITIONS and RGB_ENCODED_COLORS
+    #         key = list(RGB_COLOR_DEFINITIONS.keys())[list(RGB_COLOR_DEFINITIONS.values()).index(color_name)]
+    #
+    #         if self.setDateFontColorButton.isActiveWindow():
+    #             self.user_data['date_font_color'] = RGB_ENCODED_COLORS[key]
+    #             print(self.user_data)
+    #         elif self.setDateBackColorButton.isActiveWindow():
+    #             self.user_data['date_back_color'] = RGB_ENCODED_COLORS[key]
+    #             print(self.user_data)
+    #         elif self.setClockFontButton.isActiveWindow():
+    #             self.user_data['clock_font_color'] = RGB_ENCODED_COLORS[key]
+    #             print(self.user_data)
+    #         elif self.setClockBackButton.isActiveWindow():
+    #             self.user_data['clock_back_color'] = RGB_ENCODED_COLORS[key]
+    #             print(self.user_data)
+
 
     def generateButtons(self):
         self.setCOMPortButton = QPushButton("Настроить COM-порт")
@@ -83,6 +143,7 @@ class GUI(QWidget):
         self.showPackageInfoButton = QPushButton("Показать содержимое пакета")
         self.showPackageInfoButton.setDisabled(1)
 
+
     def setButtonsActivities(self):
         self.setCOMPortButton.clicked.connect(self.showCOMDialog)
 
@@ -93,6 +154,7 @@ class GUI(QWidget):
         self.setDateBackColorButton.clicked.connect(self.showColorDialog)
         self.setClockFontButton.clicked.connect(self.showColorDialog)
         self.setClockBackButton.clicked.connect(self.showColorDialog)
+
 
     def setGrid(self):
         grid = QGridLayout()
